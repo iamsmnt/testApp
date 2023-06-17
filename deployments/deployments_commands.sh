93edfbc4b24d075c1a8e5a5c020a1b6a65e1d461
Somnath Mahato <somnathmahato723@gmail.com>
2023-06-17 20:02:49 +0530

    deployments commans added

---
+++ deployments/deployments_commands.sh
@@ -0,0 +1,12 @@
+#installing jenkins
+brew install jenkins-lts
+
+
+#Start the Jenkins service
+brew services start jenkins-lts
+
+#restart the Jenkins service
+brew services restart jenkins-lts
+
+#Update the Jenkins version
+brew upgrade jenkins-lts
